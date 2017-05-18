783ad03940da0425b8d95c7ee4d3daf1879591eb
Aaron Piotrowski <aaron@trowski.com>
2017-05-17 22:59:17 -0500

    Use separate pool for each parallel test
    Shutdown the pool after each test to avoid spawning many workers.

--- test/ParallelDriverTest.php
+++ test/ParallelDriverTest.php
@@ -2,12 +2,27 @@
 
 namespace Amp\File\Test;
 
+use Amp\Loop;
 use Amp\Parallel\Worker\DefaultPool;
 
 class ParallelDriverTest extends DriverTest {
+    /** @var \Amp\Parallel\Worker\Pool */
+    private $pool;
+
+    public function setUp() {
+        $this->pool = new DefaultPool;
+        $this->pool->start();
+    }
+
+    public function tearDown() {
+        Loop::run(function () {
+            yield $this->pool->shutdown();
+        });
+    }
+
     protected function lRun(callable $cb) {
         \Amp\Loop::run(function() use ($cb) {
-            \Amp\File\filesystem(new \Amp\File\ParallelDriver(new DefaultPool));
+            \Amp\File\filesystem(new \Amp\File\ParallelDriver($this->pool));
             \Amp\Promise\rethrow(new \Amp\Coroutine($cb()));
         });
     }
