d0069fa23d83ee98db596789a5b1bc4c8482b293
Scott Pringle <pringlesc91@gmail.com>
2017-10-11 13:12:44 +0100

    Use "e" instead of "b" for opening files, also check that e is a valid file mode

--- lib/Internal/FileTask.php
+++ lib/Internal/FileTask.php
@@ -65,13 +65,14 @@ class FileTask implements Task {
                     case "x+":
                     case "c":
                     case "c+":
+                    case "e":
                         break;
 
                     default:
                         throw new \Error("Invalid file mode");
                 }
 
-                $handle = @\fopen($path, $mode . 'b');
+                $handle = @\fopen($path, $mode . 'e');
 
                 if (!$handle) {
                     $message = 'Could not open the file.';
