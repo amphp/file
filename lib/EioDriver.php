cbec23a472241093ba05c276978fc0615d156d44
Manuele Menozzi <manuele.menozzi@gmail.com>
2018-03-17 10:56:06 +0100

    Fixes mkdir default mode even on drivers implementations

--- lib/EioDriver.php
+++ lib/EioDriver.php
@@ -349,7 +349,7 @@ class EioDriver implements Driver {
     /**
      * {@inheritdoc}
      */
-    public function mkdir(string $path, int $mode = 0644, bool $recursive = false): Promise {
+    public function mkdir(string $path, int $mode = 0777, bool $recursive = false): Promise {
         $deferred = new Deferred;
         $this->poll->listen($deferred->promise());
 
