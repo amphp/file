5c4bd0cdfd02793926dffc0346ed215de172fa38
Daniel Leech <dan.t.leech@gmail.com>
2020-04-13 11:33:05 +0100

    Add getter

--- src/PathDoesNotExistException.php
+++ src/PathDoesNotExistException.php
@@ -4,8 +4,16 @@ namespace Amp\File;
 
 class PathDoesNotExistException extends FileSystemException
 {
+    private $path;
+
     public function __construct(string $path)
     {
+        $this->path = $path;
         parent::__construct(sprintf('Specified path "%s" does not exist', $path));
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
     }
 }
